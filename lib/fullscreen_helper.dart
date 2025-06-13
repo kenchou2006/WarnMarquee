484b5be1511414e6af9bb9727d57b55de81df269
周恭煥 <kenchou2006@gmail.com>
2025-06-13 16:08:50 +0800

    Android Full Screen Support

--- lib/fullscreen_helper.dart
+++ lib/fullscreen_helper.dart
@@ -1,2 +1,3 @@
 export 'fullscreen_helper_stub.dart'
-    if (dart.library.html) 'fullscreen_helper_web.dart';
+    if (dart.library.html) 'fullscreen_helper_web.dart'
+    if (dart.library.io) 'fullscreen_helper_android.dart';
---
+++ lib/fullscreen_helper_android.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/services.dart';
+
+bool _isFullscreen = false;
+final List<void Function()> _listeners = [];
+
+void enterFullscreen() {
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  _isFullscreen = true;
+  for (final cb in _listeners) {
+    cb();
+  }
+}
+
+void exitFullscreen() {
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  _isFullscreen = false;
+  for (final cb in _listeners) {
+    cb();
+  }
+}
+
+bool isCurrentlyFullscreen() => _isFullscreen;
+
+void addFullscreenChangeListener(void Function() cb) {
+  if (!_listeners.contains(cb)) {
+    _listeners.add(cb);
+  }
+}
